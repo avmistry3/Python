713337cea40b335df75d34abfe71f8a79ebaf052
Archana Mistry <avmistry3@gmail.com>
2024-01-04 16:43:57 -0500

    inset the values in table

--- mysql.sql
+++ mysql.sql
@@ -1,5 +1,7 @@
 --connect to cloud SQL instance
 --gcloud sql connect archana --user=root
+
+
 create databases HR;
 show databases;
 use HR;
@@ -11,9 +13,9 @@ CREATE TABLE departments(
      dept_id CHAR(5) PRIMARY KEY,
 	 dept_name VARCHAR(40),
 	 dept_mgr VARCHAR(40)
-	 )
+	 );
    
-  
+
   CREATE TABLE address(
     emp_address_id VARCHAR(10) PRIMARY KEY,
 	emp_add1 VARCHAR(50),
@@ -21,7 +23,9 @@ CREATE TABLE departments(
 	emp_zipcode VARCHAR(7),
 	emp_city VARCHAR(50),
 	emp_state VARCHAR(50),
-	)
+	);
+	
+
  
 CREATE TABLE employee(
   emp_id INTEGER(11) NOT NULL,
@@ -32,23 +36,54 @@ CREATE TABLE employee(
   hire_date DATE,
   PRIMARY KEY (emp_id),
   FOREIGN KEY(dept_id) REFERENCES departments (dept_id),
-  FOREIGN KEY (emp_address_id) REFERENCES address(ep_address_id)
-  )
+  FOREIGN KEY(emp_address_id) REFERENCES address(emp_address_id)
+  );
   
   
   
  CREATE TABLE email(
  emp_id INTEGER(11)PRIMARY KEY,
- email_1(20),
- email_2(20)
- )
+ email_1 VARCHAR(20),
+ email_2 VARCHAR(20) 
+ );
+  
+ 
  
  CREATE TABLE phone(
  emp_id INTEGER(11) PRIMARY KEY,
- emp_ph_home(10),
- emp_ph_cell(10),
- emp_ph_work(10)
- )
-
+ emp_ph_home VARCHAR(10),
+ emp_ph_cell VARCHAR(10),
+ emp_ph_work VARCHAR (10)
+ );
+ 
 show tables;
 desc employee;
+
+INSERT INTO departments ( dept_id,dept_name,dept_mgr)
+    VALUES ( "sal01" , "sales","john"); 
+INSERT INTO departments ( dept_id,dept_name,dept_mgr)
+    VALUES ( "mkl01" , "marketing","Peter");
+INSERT INTO departments ( dept_id,dept_name,dept_mgr)
+    VALUES ( "inv01","inventory" , "alex");
+	
+	
+INSERT INTO address ( emp_address_id,emp_add1,emp_add2,emp_zipcode,emp_city,emp_state)
+    VALUES ( "eadd01" , "75", "queen st","l7p439","brampton","ON"); 	
+
+INSERT INTO employee ( emp_id,first_name,last_name,emp_address_id,dept_id,hire_date)
+    VALUES ( "001" , "kelly", "smith","eadd01","sal01","2023-05-23"); 
+
+INSERT INTO email( emp_id,email_1,email_2)
+    VALUES ( "001" , "k.smith@gmail.com", "k.smith34@yahoo.ca");
+
+ 
+ INSERT INTO phone ( emp_id,emp_ph_home,emp_ph_cell,emp_ph_work)
+     VALUES ( "001" , "4371234575", "6475671234","9057894561"); 
+	
+	Select * from departments;
+	select * from address;
+	select * from employee;
+	SELECT * from email;
+	SELECT * from phone;
+	
+	
