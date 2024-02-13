72e634b95422a94b9be8d6015602794c8bbd469f
Archana Mistry <avmistry3@gmail.com>
2024-02-13 00:14:19 -0500

    emp table is added

--- mysql.sql
+++ mysql.sql
@@ -71,7 +71,7 @@ INSERT INTO address ( emp_address_id,emp_add1,emp_add2,emp_zipcode,emp_city,emp_
     VALUES ( "eadd01" , "75", "queen st","l7p439","brampton","ON"); 	
 
 INSERT INTO employee ( emp_id,first_name,last_name,emp_address_id,dept_id,hire_date)
-    VALUES ( "001" , "kelly", "smith","eadd01","sal01","2023-05-23"); 
+    VALUES ( "001" , "kelly", "smith","eadd01","sal01","2023-05-23");
 
 INSERT INTO email( emp_id,email_1,email_2)
     VALUES ( "001" , "k.smith@gmail.com", "k.smith34@yahoo.ca");
@@ -86,4 +86,26 @@ INSERT INTO email( emp_id,email_1,email_2)
 	SELECT * from email;
 	SELECT * from phone;
 	
-	
+	CREATE TABLE emp(
+  emp_id INTEGER(11) NOT NULL,
+  first_name VARCHAR(15),
+  last_name VARCHAR(20),
+  dept_id INTEGER(5) NOT NULL,
+  salary INTEGER
+  );
+
+
+INSERT INTO emp ( emp_id,first_name,last_name,dept_id,salary)
+    VALUES ("1","Paresh","Yadav","1","10");
+INSERT INTO emp ( emp_id,first_name,last_name,dept_id,salary)
+    VALUES ("2","Gita","Yadav","2","20");
+INSERT INTO emp ( emp_id,first_name,last_name,dept_id,salary)
+    VALUES ("3","Poonam","Sharma","2","30");
+INSERT INTO emp ( emp_id,first_name,last_name,dept_id,salary)
+    VALUES ("4","Bhakti","Patel","3","40");
+INSERT INTO emp ( emp_id,first_name,last_name,dept_id,salary)
+    VALUES ("5","Maunee","Patel","1","40");
+INSERT INTO emp ( emp_id,first_name,last_name,dept_id,salary)
+    VALUES ("6","Archana","Mistry","4","50");
+INSERT INTO emp ( emp_id,first_name,last_name,dept_id,salary)
+    VALUES ("7","Raunakraj","Patel","5","60");
